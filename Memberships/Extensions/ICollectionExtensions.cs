bb2ff74e5c3ec6ff9509fa2ed4fa487a7cad905c
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-08-04 17:30:39 -0400

    Finished create and edit views in chap5, but edit view caused error, doesn't seem to be database info yet, but haven't found the error.

---
+++ Memberships/Extensions/ICollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memberships.Extensions
+{
+    public static class ICollectionExtensions
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this ICollection<T>items, int selectedValue)
+        {
+            return from item in items
+                   select new SelectListItem
+                   {
+                       // Cannot get props directly bc item is of type T
+                       // Solving with reflection; calling GetPropertyValue method
+                       // to return a string containing the property value
+                       Text = item.GetPropertyValue("Title"),
+                       Value = item.GetPropertyValue("Id"),
+                       Selected = item.GetPropertyValue("Id")
+                       .Equals(selectedValue.ToString())
+                   };
+        }
+    }
+}
