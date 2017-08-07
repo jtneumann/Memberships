dee2dfee6166ae1cc347030a07de7b3ed7378583
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-08-07 09:56:28 -0400

    Product controller and CRUD views built - ansyc work needed for Product Index view though.

---
+++ Memberships/Areas/Admin/Models/ProductModel.cs
@@ -0,0 +1,51 @@
+﻿using Memberships.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Areas.Admin.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Display(Name = ("Image Url"))]
+        [MaxLength(1024)]
+        public string ImageUrl { get; set; }
+
+        [MaxLength(2048)]
+        public string Description { get; set; }
+
+        public int ProductLinkTextId { get; set; }
+
+        public int ProductTypeId { get; set; }
+
+        [Display(Name = "Product Link Texts")]
+        public ICollection<ProductLinkText> ProductLinkTexts { get; set; }
+
+        [Display(Name = "Product Types")]
+        public ICollection<ProductType> ProductTypes { get; set; }
+
+        public string ProductType
+        {
+            get
+            {
+                return ProductTypes == null || ProductTypes.Count.Equals(0) ?
+                    string.Empty : ProductTypes.First(pt => pt.Id.Equals(ProductTypeId)).Title;
+            }
+        }
+
+        public string ProductLinkText
+        {
+            get {
+                return ProductLinkTexts == null || ProductLinkTexts.Count(0) ?
+                    string.Empty : ProductLinkTexts.First(plt => plt.Id.Equals(ProductLinkTexts)).Title;
+            }
+        }
+    }
+}
