e9bb9c3d8722050127e0a030f0ced429d7767902
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-08-08 17:26:47 -0400

    ProductItem controller and view through Edit get action, which has a current unresolved error in the action return statement - line 82.

---
+++ Memberships/Areas/Admin/Models/ProductItemModel.cs
@@ -0,0 +1,26 @@
+﻿using Memberships.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Areas.Admin.Models
+{
+    public class ProductItemModel
+    {
+        [Display(Name = "Product Id")]
+        public int ProductId { get; set; }
+        [Display(Name = "Item Id")]
+        public int ItemId { get; set; }
+        [Display(Name = "Product")]
+        public string ProductTitle { get; set; }
+        [Display(Name = "Item")]
+        public string ItemTitle { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public ICollection<Item> Items { get; set; }
+
+    }
+}
