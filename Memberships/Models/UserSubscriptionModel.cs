c1e7e6dae7cc6e28f9cb65bdef767ce6b38230a3
Jeff <jeff.neumann@phoenix.edu>
2017-08-18 15:14:54 -0400

    Subscriptions view complete with ID error - may need subscription actions to resolve though.

---
+++ Memberships/Models/UserSubscriptionModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Models
+{
+    public class UserSubscriptionModel
+    {
+        [MaxLength(255)]
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(2048)]
+        public string Description { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Registration Code")]
+        public string RegistrationCode { get; set; }
+
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+    }
+}
