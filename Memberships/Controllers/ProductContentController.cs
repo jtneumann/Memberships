89ca30ce8ab54b6c72fd07ec0779aefaa3a2f3df
Jeff <jeff.neumann@phoenix.edu>
2017-08-22 21:33:45 -0400

    ProductContentController and corresponding classes added.

---
+++ Memberships/Controllers/ProductContentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace Memberships.Controllers
+{
+    [Authorize]
+    public class ProductContentController : Controller
+    {
+        // GET: ProductContent
+        public async Task<ActionResult> Index(int id)
+        {
+            return View();
+        }
+    }
+}
