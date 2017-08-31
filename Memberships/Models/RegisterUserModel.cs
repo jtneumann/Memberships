c756ce7cd1209ae5d3219159f6122ba0aabfa3f9
Jeff <jeff.neumann@phoenix.edu>
2017-08-30 20:41:38 -0400

    Added RegisterUserModel and call to _RegisterCodePartial in the Home/index view.

---
+++ Memberships/Models/RegisterUserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Models
+{
+    public class RegisterUserModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be at least {1} characters long.",
+            MinimumLength = 2)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {1} characters long. Have 1 non-letter, 1 digit, 1 uppercase('A'-'Z').", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        public bool AcceptUserAgreement { get; set; }
+    }
+}
